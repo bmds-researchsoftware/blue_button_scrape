47e6d874f007d1d8234cfaf706df78bdb6cad9a2
Will Haslett <will.haslett@gmail.com>
2019-04-24 11:46:27 -0600

    Working on converting auth code to token

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ from flask import request
 import requests
 app = Flask(__name__)
 
-@app.route('/get_auth_code')
-def get_auth_code():
-    return request.args.get('code')
+CLIENT_ID = 'c3IP1q7AktCVRKQa7QpwRXI7Hhqo8izlgtx6UsGD'
+CLIENT_SECRET = 'bqJucbcrMhD3oKYOkSDsMBlhOX4oLMUCfYPKX8Jq8nN3d6IMjRixIkSLqbB7nEwCQpVzcVVqeoQGSWJJ8OYGfXgwrkFNFhnvZnGsBGVuWrWESlFxT4zQcAXFy63jvKX7'
+
+# Callback from manuaally entered authorization GET request
+@app.route('/get_token')
+def get_token():
+    auth_code = request.args.get('code')
+    post_url = 'https://sandbox.bluebutton.cms.gov/v1/o/token/'
+    post_payload = {
+            'code': auth_code,
+            'grant_type': 'authorization_code',
+            'redirect_uri': 'http://localhost:5000/get_data' }
+    response = requests.post(post_url, auth=(CLIENT_ID, CLIENT_SECRET), data=post_payload)
+    print(response.text)
+    return 'foo'
+
+# Callback from get_token
+@app.route('/get_data')
+def get_data():
+    return 'bar'
