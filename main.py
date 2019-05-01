2d624e5585583f1b6e1d0982c67e5c9623f5a6a9
Will Haslett <will.haslett@gmail.com>
2019-05-01 11:53:23 -0400

    Getting access token works

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ CLIENT_SECRET = 'bqJucbcrMhD3oKYOkSDsMBlhOX4oLMUCfYPKX8Jq8nN3d6IMjRixIkSLqbB7nEw
 # Callback from manuaally entered authorization GET request
 @app.route('/get_token')
 def get_token():
+
     auth_code = request.args.get('code')
-    post_url = 'https://sandbox.bluebutton.cms.gov/v1/o/token/'
-    post_payload = {
-            'code': auth_code,
-            'grant_type': 'authorization_code',
-            'redirect_uri': 'http://localhost:5000/get_data' }
-    response = requests.post(post_url, auth=(CLIENT_ID, CLIENT_SECRET), data=post_payload)
-    print(response.text)
-    return 'foo'
+    if auth_code:
+        post_url = 'https://sandbox.bluebutton.cms.gov/v1/o/token/'
+        post_payload = {
+                'code': auth_code,
+                'grant_type': 'authorization_code',
+                'redirect_uri': 'http://localhost:5000/get_token' }
+        response = requests.post(post_url, auth=(CLIENT_ID, CLIENT_SECRET), data=post_payload)
+        print(response.text)
+        return 'getting token'
+    else:
+        return 'got token'
 
 # Callback from get_token
 @app.route('/get_data')
